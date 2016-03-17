c888a924699be211bd1d6cf406d5ee07f750cedb
Your Name <you@example.com>
2016-03-18 00:16:39 +0800

    #jexus

--- Booker.Owin/SiteRootPath.cs
+++ Booker.Owin/SiteRootPath.cs
@@ -13,8 +13,22 @@ namespace Booker
         /// <summary>
         /// 网站根文件夹物理路径(for tinyfox)
         /// </summary>
-        static readonly string _RootPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../");
+        static string _rootPath;
 
+        static SiteRootPath()
+        {
+            var pcDebugPath = @"C:\Users\ss222\Documents\Booker\Booker";
+            var appPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "views");
+            var evnPath = Path.Combine(Environment.CurrentDirectory, "views");
+            if (Directory.Exists(pcDebugPath))
+                _rootPath = pcDebugPath;
+            else if (Directory.Exists(appPath))
+                _rootPath = appPath;
+            else if (Directory.Exists(evnPath))
+                _rootPath = appPath;
+            else
+                File.WriteAllText("path.log", string.Format("appPath:{0}\r\nevnPath:{1}", appPath, evnPath));
+        }
 
         /// <summary>
         /// 获取网站或WEB应用的根文件夹的物理路径
@@ -22,8 +36,7 @@ namespace Booker
         /// <returns></returns>
         public string GetRootPath()
         {
-            return _RootPath;
-
+            return _rootPath;
         }
 
     }
