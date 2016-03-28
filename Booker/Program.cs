6d5c5421b45ef304d78d6753e3e82757e285792f
ss22219 <ss22219@qq.com>
2016-03-28 11:36:06 +0800

    #fastcgi

--- Booker/Program.cs
+++ Booker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Owin.Hosting;
+using Fos;
 namespace Booker
 {
     class MainClass
@@ -7,13 +8,36 @@ namespace Booker
         public static void Main(string[] args)
         {
             LoadAssemblys();
+            if (args != null && args.Length > 0 && args[0].ToLower().IndexOf("fastcgi") != -1)
+            {
+                int port = 0;
+                if (args.Length <= 1 || !int.TryParse(args[1], out port))
+                    port = 9000;
+                FastCgi(port);
+            }
+            else
+                SelfHost();
+        }
 
+        public static void FastCgi(int port)
+        {
+            var startup = new Startup();
+            using (var fosServer = new FosSelfHost(startup.Configuration))
+            {
+                fosServer.Bind(System.Net.IPAddress.Loopback, port);
+                fosServer.Start(false);
+            }
+        }
+
+        public static void SelfHost()
+        {
             string url = "http://+:8080";
             WebApp.Start<Startup>(url);
             Console.WriteLine("server is start on {0}", url);
             Console.WriteLine("press enter to exit");
             Console.ReadLine();
         }
+
 
         /// <summary>
         /// RazorEngine,Booker.Module
